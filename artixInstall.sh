7198306c66dd76dd6d864488dbc467fbe7361273
Adamina <aaadamm01@gmail.com>
2026-07-13 02:00:43 -0400

    Added support for real hardware or VM, also a couple of minor tweaks.

--- artixInstall.sh
+++ artixInstall.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 clear
+user="vmuser"
+echo -e "Automated Artix Installer by Adamina02\n\nSelect install type:\n  1: Virtual machine\n  2: Real hardware"
+read -p "Option> " opt
+if [[ $opt -eq 2 ]]; then
+	user="adamina"
+fi
+sleep 1s
+
 echo "Partitioning disks..."
-echo -e "label: gpt\ndevice: /dev/vda\n\n/dev/vda1 : start=2048, size=1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B\n/dev/vda2 : start=1050624, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sfdisk /dev/vda
+if [[ $opt -eq 2 ]]; then
+	echo -e "label: gpt\ndevice: /dev/nvme0n1\n\n/dev/nvme0n1p1 : start=2048, size=1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B\n/dev/nvme0n1p2 : start=1050624, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sfdisk /dev/nvme0n1
+	sleep 0.1s
+	echo -e "label: gpt\ndevice: /dev/sda\n\n/dev/sda1 : start=2048, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sfdisk /dev/sda
+else
+	echo -e "label: gpt\ndevice: /dev/vda\n\n/dev/vda1 : start=2048, size=1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B\n/dev/vda2 : start=1050624, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4" | sfdisk /dev/vda
+fi
 sleep 1s
 
 echo "Creating filesystems..."
-mkfs.vfat -F 32 /dev/vda1
-sleep 0.1s
-mkfs.xfs /dev/vda2
+if [[ $opt -eq 2 ]]; then
+	mkfs.vfat -F 32 /dev/nvme0n1p1
+	sleep 0.1s
+	mkfs.xfs /dev/nvme0n1p2
+	sleep 0.1s
+	mkfs.xfs /dev/sda1
+else
+	mkfs.vfat -F 32 /dev/vda1
+	sleep 0.1s
+	mkfs.xfs /dev/vda2
+fi
 sleep 1s
 
 echo "Mounting filesystems..."
-mount /dev/vda2 /mnt
+if [[ $opt -eq 2 ]]; then
+	mount /dev/nvme0n1p2 /mnt
+else
+	mount /dev/vda2 /mnt
+fi
 sleep 0.1s
 mkdir -p /mnt/boot
 sleep 0.1s
-mount /dev/vda1 /mnt/boot
+if [[ $opt -eq 2 ]]; then
+	mount /dev/nvme0n1p1 /mnt/boot
+else
+	mount /dev/vda1 /mnt/boot
+fi
 sleep 1s
 
 echo "Installing base system..."
@@ -23,12 +53,30 @@ basestrap /mnt 7zip acpid-dinit amd-ucode android-tools base blueman bluez-dinit
 sleep 1s
 
 echo "Setting up fstab..."
-echo -e "/dev/vda1 /boot vfat umask=0077,tz=UTC 0 2\n/dev/vda2 / xfs defaults,noatime 0 1" > /mnt/etc/fstab
+if [[ $opt -eq 2 ]]; then
+	echo -e "/dev/nvme0n1p1 /boot vfat umask=0077,tz=UTC 0 2\n/dev/nvme0n1p2 / xfs defaults,noatime 0 1\n/dev/sda1 /mnt/hdd xfs defaults,noatime,nofail 0 2" > /mnt/etc/fstab
+else
+	echo -e "/dev/vda1 /boot vfat umask=0077,tz=UTC 0 2\n/dev/vda2 / xfs defaults,noatime 0 1" > /mnt/etc/fstab
+fi
 sleep 1s
 
 echo "Entering chroot..."
 artix-chroot /mnt
 sleep 1s
+
+if [[ $opt -eq 2 ]]; then
+	echo "Creating additional mount points..."
+	mkdir -p /mnt/hdd
+	sleep 0.1s
+	chmod -R 777 /mnt/hdd
+	sleep 0.1s
+	mkdir -p /mnt/usb1
+	sleep 0.1s
+	mkdir -p /mnt/usb2
+	sleep 0.1s
+	mkdir -p /mnt/usb3
+	sleep 1s
+fi
 
 echo "Settting up pacman options..."
 echo -e "[options]\nHookDir = /etc/pacman.d/hooks/\nHoldPkg = pacman glibc\nArchitecture = auto\nIgnorePkg = elogind lib32-elogind lib32-polkit polkit sudo\nColor\nCheckSpace\nVerbosePkgLists\nParallelDownloads = 16\nDownloadUser = alpm\nSigLevel = Required DatabaseOptional\nLocalSigLevel = Optional\n[system]\nInclude = /etc/pacman.d/mirrorlist\n[world]\nInclude = /etc/pacman.d/mirrorlist\n[galaxy]\nInclude = /etc/pacman.d/mirrorlist\n[lib32]\nInclude = /etc/pacman.d/mirrorlist" > /etc/pacman.conf
@@ -53,11 +101,15 @@ echo -e "LANG=C.UTF-8" > /etc/locale.conf
 sleep 1s
 
 echo "Creating EFI stub entry..."
-efibootmgr -c -d /dev/vda -l /vmlinuz-linux-rt -L "Artix Linux" -p 1 -u 'amdgpu.ppfeaturemask=0xffffffff clocksource=tsc initrd=\initramfs-linux-rt.img loglevel=4 mitigations=off root=/dev/vda2 rw sysctl.fs.file-max=10485760 sysctl.vm.max_map_count=1048576 sysctl.vm.swappiness=10 tsc=reliable'
+if [[ $opt -eq 2 ]]; then
+	efibootmgr -c -d /dev/nvme0n1 -l /vmlinuz-linux-rt -L "Artix Linux" -p 1 -u 'amdgpu.ppfeaturemask=0xffffffff clocksource=tsc initrd=\initramfs-linux-rt.img loglevel=4 mitigations=off root=/dev/nvme0n1p2 rw sysctl.fs.file-max=10485760 sysctl.vm.max_map_count=1048576 sysctl.vm.swappiness=10 tsc=reliable'
+else
+	efibootmgr -c -d /dev/vda -l /vmlinuz-linux-rt -L "Artix Linux" -p 1 -u 'amdgpu.ppfeaturemask=0xffffffff clocksource=tsc initrd=\initramfs-linux-rt.img loglevel=4 mitigations=off root=/dev/vda2 rw sysctl.fs.file-max=10485760 sysctl.vm.max_map_count=1048576 sysctl.vm.swappiness=10 tsc=reliable'
+fi
 sleep 1s
 
 echo "Adding user account..."
-useradd -G audio,disk,input,network,power,seat,storage,tty,users,video,wheel -m vmuser
+useradd -G audio,disk,input,network,power,seat,storage,tty,users,video,wheel -m $user
 sleep 1s
 
 echo "Setting up doas..."
@@ -77,9 +129,9 @@ echo -e "EDITOR=nano\nLD_BIND_NOW=1\nQT_QPA_PLATFORMTHEME=gtk2" > /etc/environme
 sleep 1s
 
 echo "Setting up TTY..."
-echo -e 'ACTIVE_CONSOLES="/dev/tty1"' > /etc/dinit.d/config/console.conf
+echo -e "ACTIVE_CONSOLES=\"/dev/tty1\"" > /etc/dinit.d/config/console.conf
 sleep 0.1s
-echo -e 'GETTY_ARGS="-a vmuser -J"\nGETTY_BAUD=38400\nGETTY_TERM=linux' > /etc/dinit.d/config/agetty-tty1.conf
+echo -e "GETTY_ARGS=\"-a $user -J\"\nGETTY_BAUD=38400\nGETTY_TERM=linux" > /etc/dinit.d/config/agetty-tty1.conf
 sleep 1s
 
 echo "Setting up turnstile..."
@@ -87,17 +139,17 @@ echo -e "debug = no\nbackend = dinit\ndebug_stderr = no\nlinger = no\nrundir_pat
 sleep 1s
 
 echo "Setting up mkinitcpio..."
-echo -e 'MODULES=(nct6683 ntsync)\nBINARIES=()\nFILES=()\nHOOKS=(base udev autodetect microcode modconf kms keyboard keymap block filesystems fsck)\nCOMPRESSION="zstd"\nCOMPRESSION_OPTIONS=()\nMODULES_DECOMPRESS="no"' > /etc/mkinitcpio.conf
+echo -e "MODULES=(nct6683 ntsync)\nBINARIES=()\nFILES=()\nHOOKS=(base udev autodetect microcode modconf kms keyboard keymap block filesystems fsck)\nCOMPRESSION=\"zstd\"\nCOMPRESSION_OPTIONS=()\nMODULES_DECOMPRESS=\"no\"" > /etc/mkinitcpio.conf
 sleep 0.1s
 mkinitcpio -P
 sleep 1s
 
 echo "Setting up ZRAM..."
-echo -e "ZRAM_COMP_ALGORITHM=zstd\nZRAM_MAX_SIZE=8192\nZRAM_PRIORITY=32767\nZRAM_SIZE=100\nZRAMEN_QUIET=1\nZRAMEN_SWAPON_DISCARD=true" > /etc/dinit.d/config/zramen.conf
+echo -e "ZRAM_COMP_ALGORITHM=zstd\nZRAM_MAX_SIZE=8192\nZRAM_PRIORITY=32767\nZRAM_SIZE=32\nZRAMEN_QUIET=1\nZRAMEN_SWAPON_DISCARD=true" > /etc/dinit.d/config/zramen.conf
 sleep 1s
 
 echo "Setting file descriptor limits..."
-echo -e "vmuser hard nofile 1048576\nvmuser soft nofile 1048576" > /etc/security/limits.conf
+echo -e "$user hard nofile 1048576\n$user soft nofile 1048576" > /etc/security/limits.conf
 sleep 1s
 
 echo "Setting up services..."
@@ -121,29 +173,30 @@ ln -s /etc/dinit.d/turnstiled /etc/dinit.d/boot.d/turnstiled
 sleep 0.1s
 ln -s /etc/dinit.d/zramen /etc/dinit.d/boot.d/zramen
 sleep 0.1s
-su vmuser -c "mkdir -p /home/vmuser/.config/dinit.d/boot.d"
+su $user -c "mkdir -p /home/$user/.config/dinit.d/boot.d"
 sleep 0.1s
-ln -s /etc/dinit.d/user/dbus /home/vmuser/.config/dinit.d/boot.d/dbus
+ln -s /etc/dinit.d/user/dbus /home/$user/.config/dinit.d/boot.d/dbus
 sleep 0.1s
-ln -s /etc/dinit.d/user/pipewire /home/vmuser/.config/dinit.d/boot.d/pipewire
+ln -s /etc/dinit.d/user/pipewire /home/$user/.config/dinit.d/boot.d/pipewire
 sleep 0.1s
-ln -s /etc/dinit.d/user/pipewire-pulse /home/vmuser/.config/dinit.d/boot.d/pipewire-pulse
+ln -s /etc/dinit.d/user/pipewire-pulse /home/$user/.config/dinit.d/boot.d/pipewire-pulse
 sleep 0.1s
-ln -s /etc/dinit.d/user/wireplumber /home/vmuser/.config/dinit.d/boot.d/wireplumber
+ln -s /etc/dinit.d/user/wireplumber /home/$user/.config/dinit.d/boot.d/wireplumber
 sleep 1s
 
 echo "Updating pacman databases..."
 pacman -Syu
 sleep 1s
 
-echo "Installing 32-bit applications..."
+echo "Installing additional applications..."
 pacman -S --noconfirm lib32-mesa lib32-vulkan-mesa-layers lib32-vulkan-radeon steam --assume-installed lib32-elogind
+sleep 0.1s
+pacman -S --noconfirm mugshot xfce4-whiskermenu-plugin --assume-installed polkit
 sleep 1s
 
 echo "Running some commands as user..."
-su vmuser -c "xdg-user-dirs-update"
+su $user -c "xdg-user-dirs-update"
 sleep 0.1s
-su vmuser -c 'echo -e "xfce4-panel &\nxfce4-screensaver &\nxfdesktop &\nblueman-applet &\nconnman-gtk --tray &\nthunar --daemon &\nexec xfwm4" > /home/vmuser/.xinitrc'
+su $user -c 'echo -e "xfce4-panel &\nxfce4-screensaver &\nxfdesktop &\nblueman-applet &\nconnman-gtk --tray &\nthunar --daemon &\nexec xfwm4" > /home/$user/.xinitrc'
 sleep 0.1s
-su vmuser -c 'echo -e "[[ -f ~/.bashrc ]] && . ~/.bashrc\nstartx" > /home/vmuser/.bash_profile'
-clear
+su $user -c 'echo -e "[[ -f ~/.bashrc ]] && . ~/.bashrc\nstartx" > /home/$user/.bash_profile'
